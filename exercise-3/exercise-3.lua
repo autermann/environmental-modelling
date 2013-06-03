f277c0f58bd0585e3e00f8c1551c23e84e155b5f
Christian Autermann <christian@autermann.org>
2013-06-03 23:24:42 +0200

    first version of exercise 3

---
+++ exercise-3/exercise-3.lua
@@ -0,0 +1,108 @@
+math.randomseed(os.time())
+
+EMPTY = 0
+FOREST = 1
+BURNING = 2
+BURNED = 3
+
+function distribution(cs)
+	local d = {}
+	forEachCell(cs, function(c)
+		if d[c.cover] then 
+			d[c.cover] = d[c.cover] + 1
+		else d[c.cover] = 1 end
+	end)
+	local size = cs.xdim * cs.ydim
+	for k,v in pairs(d) do d[k] = v/size end
+	return d
+end
+
+function simulate(simulations, xdim, ydim, ntype, burningIterations, pBurn)
+	function sim(initialCover)
+		local world = CellularSpace{ xdim = xdim, ydim = ydim }
+		world:createNeighborhood{ strategy = ntype, self = false }
+
+		forEachCell(world, function(cell)
+			cell.iteration = 0
+			if math.random() > initialCover then
+				cell.cover = EMPTY
+			else
+				cell.cover = FOREST
+			end
+		end)
+
+		update = function(cs)
+			local burning = false
+			forEachCell(cs, function(cell)
+				if cell.past.cover == FOREST then
+					if math.random() <= pBurn then
+						forEachNeighbor(cell, function(cell, neighbor)
+							if neighbor.past.cover == BURNING then
+								burning = true
+								cell.cover = BURNING
+								cell.iteration = 1
+							end
+						end)
+					end
+				elseif cell.past.cover == BURNING then
+					cell.iteration = cell.past.iteration + 1
+					if cell.iteration >= burningIterations then
+						cell.cover = BURNED
+					else
+						burning = true
+					end
+				end
+			end)
+			return burning
+		end
+		world:sample().cover = BURNING
+		Timer{Event{action = function(e)
+			world:synchronize()
+			if not update(world) then
+				world.finished = e:getTime()
+				return false
+			end
+		end}}:execute(150)
+
+		return {
+			runtime = world.finished,
+			hist = distribution(world)
+		}
+	end
+	local sims = {}
+	for i = 1, simulations do
+		table.insert(sims, sim(math.random())) 
+	end
+	return sims
+end
+
+
+-- execution
+local worlds = {
+	simulate(5,  50,  50, "vonneumann", 1, 1.0),
+	simulate(5,  50,  50, "moore",      1, 1.0),
+	simulate(5,  50,  50, "vonneumann", 2, 1.0),
+	simulate(5, 100, 100, "vonneumann", 1, 1.0),
+	simulate(5,  50,  50, "vonneumann", 1, 0.9)	
+}
+print("Run\tEmpty\tForest\tBurned\tRuntime\tBurning")
+for i,runs in ipairs(worlds) do
+	local avg = { i, 0, 0, 0, 0, 0}
+	for j,run in ipairs(runs) do
+		local runtime = run.runtime or -1
+		local empty = run.hist[EMPTY] or 0
+		local forest = run.hist[FOREST] or 0
+		local burning = run.hist[BURNING] or 0
+		local burned = run.hist[BURNED] or 0
+		local l = { i .. "." .. j, empty, forest,
+					burned, runtime, burning }
+		avg[2] = avg[2] + runtime
+		avg[3] = avg[3] + empty
+		avg[4] = avg[4] + forest
+		avg[5] = avg[5] + burning
+		avg[6] = avg[6] + burned
+		print(table.concat(l,"\t"))
+	end
+	for i = 2,6 do avg[i] = avg[i]/#runs end
+	print(table.concat(avg,"\t"))
+end
